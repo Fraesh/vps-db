e65c3d1cbc2fd00aaffe967a2a7dedcc39141553
Kevin <fraesh93@gmail.com>
2021-11-21 11:07:03 +0100

    try first

---
+++ integrateChanges.js
@@ -0,0 +1,43 @@
+const updates = require("./lastUpdate.json");
+let db = require("./vpsdb.json");
+const fs = require("fs");
+
+updates.forEach((update) => {
+  switch (update.action) {
+    case "DELETE":
+      deleteGame(update);
+    case "CREATE":
+      createGame(update);
+    case "UPDATE":
+      updateGame(update);
+    default:
+      console.log("FAULTY ACTION");
+  }
+});
+
+fs.writeFile("vpsdb.json", JSON.stringify(db), console.log);
+
+const deleteGame = ({ name, year, manufacturer }) => {
+  const index = db.findIndex(
+    (g) => g.name === name && g.year === year && g.manufacturer === manufacturer
+  );
+  if (index >= 0) {
+    console.log(`DELETE ${name}-${year}-${manufacturer}`);
+    db = db.splices(index, 1);
+  }
+};
+
+const createGame = ({ data }) => {
+  db.push(data);
+  console.log(`CREATE ${data.name}-${data.year}-${data.manufacturer}`);
+};
+
+const updateGame = ({ name, year, manufacturer, data }) => {
+  const index = db.findIndex(
+    (g) => g.name === name && g.year === year && g.manufacturer === manufacturer
+  );
+  if (index >= 0) {
+    console.log(`UPDATE ${name}-${year}-${manufacturer}`);
+    db[i] = data;
+  }
+};
