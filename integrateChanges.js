370421c6f18a2a7207d56c1c894a68d5ed8dcf03
Kevin <fraesh93@gmail.com>
2021-11-28 10:39:20 +0100

    Revert "first implementation image scraper"
    This reverts commit 2bd830a7ce15cce20225400abc727b5711242cef.

--- integrateChanges.js
+++ integrateChanges.js
@@ -1,60 +1,6 @@
 const updates = require("./lastUpdate.json");
 let db = require("./vpsdb.json");
 const fs = require("fs");
-const sharp = require("sharp");
-const { default: axios } = require("axios");
-
-const download = async (url, fileName) => {
-  try {
-    const buffer = await axios.get(url, { responseType: "arraybuffer" });
-    sharp(buffer.data)
-      .resize({ height: 700 })
-      .webp({ quality: 50, reductionEffort: 6 })
-      .toFile(`img/${fileName}.webp`, (err, info) => {
-        console.log(err, info);
-      });
-    return true;
-  } catch (e) {
-    return false;
-  }
-};
-
-const getImages = async (games) => {
-  await Promise.all(
-    games.map(async (el) => {
-      if (el.tableFiles) {
-        await Promise.all(
-          el.tableFiles?.map(async (tb) => {
-            if (tb.imgUrl && !tb.imgUrl.includes("fraesh.github")) {
-              const fileName = `${el.id}_table_${new Date().getTime()}`;
-              const worked = download(tb.imgUrl, fileName);
-              console.log(fileName);
-              if (worked) {
-                tb.imgUrl = `https://fraesh.github.io/vps-db/img/${fileName}.webp`;
-              }
-            }
-          })
-        );
-      }
-      if (el.b2sFiles) {
-        await Promise.all(
-          el.b2sFiles?.map(async (tb) => {
-            if (tb.imgUrl && !tb.imgUrl.includes("fraesh.github")) {
-              const fileName = `${el.id}_b2s_${new Date().getTime()}`;
-              const worked = download(tb.imgUrl, fileName);
-              console.log(fileName);
-              if (worked) {
-                tb.imgUrl = `https://fraesh.github.io/vps-db/img/${fileName}.webp`;
-              }
-            }
-          })
-        );
-      }
-    })
-  );
-};
-
-// UPDATE DATABASE
 
 const deleteGame = ({ id }) => {
   const index = db.findIndex((g) => g.id === id);
@@ -65,14 +11,12 @@ const deleteGame = ({ id }) => {
   }
 };
 
-const createGame = async ({ data }) => {
-  await getImages(data);
+const createGame = ({ data }) => {
   db.push(data);
   console.log(`CREATE ${data.name}-${data.year}-${data.manufacturer}`);
 };
 
-const updateGame = async ({ id, data }) => {
-  await getImages(data);
+const updateGame = ({ id, data }) => {
   const index = db.findIndex((g) => g.id === id);
   if (index >= 0) {
     console.log(`UPDATE ${data.name}-${data.year}-${data.manufacturer}`);
@@ -82,25 +26,20 @@ const updateGame = async ({ id, data }) => {
   }
 };
 
-const updateDatabase = async () => {
-  await Promise.all(
-    updates.map(async (update) => {
-      switch (update.action) {
-        case "DELETE":
-          deleteGame(update);
-          break;
-        case "CREATE":
-          await createGame(update);
-          break;
-        case "UPDATE":
-          await updateGame(update);
-          break;
-        default:
-          console.log("FAULTY ACTION");
-      }
-    })
-  );
-  fs.writeFile("vpsdb.json", JSON.stringify(db), console.log);
-};
+updates.forEach((update) => {
+  switch (update.action) {
+    case "DELETE":
+      deleteGame(update);
+      break;
+    case "CREATE":
+      createGame(update);
+      break;
+    case "UPDATE":
+      updateGame(update);
+      break;
+    default:
+      console.log("FAULTY ACTION");
+  }
+});
 
-updateDatabase();
+fs.writeFile("vpsdb.json", JSON.stringify(db), console.log);
